734c10a3e27fd376435829fe9475f6550f712330
Patrick Luu <patluu66@gmail.com>
2017-11-29 18:01:16 -0800

    sequelize pets table

--- app/config/connection.js
+++ app/config/connection.js
@@ -20,34 +20,3 @@ var sequelize = new Sequelize("pets2", "root", "", {
 // Exports the connection for other files to use
 module.exports = sequelize;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Dependencies
-// var Sequelize = require("sequelize");
-
-// // Creates mySQL connection using Sequelize
-// var sequelize = new Sequelize("sequelize_chirpy", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-
-// // Exports the connection for other files to use
-// module.exports = sequelize;
