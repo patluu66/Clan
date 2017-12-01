d14bbaea44cc3374ad360bdb94306612be679712
Patrick Luu <patluu66@gmail.com>
2017-12-01 11:11:53 -0800

    clean repo

---
+++ app/models/response.js
@@ -0,0 +1,82 @@
+
+module.exports = function(sequelize, DataTypes) {
+
+	var Response = sequelize.define("Response", {
+	  threads_id: {
+	    type: DataTypes.INTEGER
+	  },
+	  owner_id: {
+	    type: DataTypes.INTEGER
+	  },
+	  response: {
+	    type: DataTypes.STRING
+	  }
+	});
+
+	Response.associate = function(models) {
+	// Associating Author with Posts
+	// When an Author is deleted, also delete any associated Posts
+		Response.belongsTo(models.Person, {
+		  foreignKey: {
+		    allowNull: false
+		  }
+		});
+	}
+  return Response;
+};
+
+
+
+
+
+
+
+
+
+
+// This may be confusing but here Sequelize (capital) references the standard library
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references our connection to the DB.
+// var sequelize = require("../config/connection.js");
+
+
+// var PostResponses = sequelize.define("reply", {
+//   threads_id: {
+//     type: Sequelize.INTEGER
+//   },
+//   owner_id: {
+//     type: Sequelize.INTEGER
+//   },
+//   response: {
+//     type: Sequelize.STRING
+//   },
+//   created_at: {
+//     type: Sequelize.DATE
+//   }
+// }, {
+//   timestamps: false
+// });
+
+
+// PostResponses.sync();
+// module.exports = PostResponses;
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function(sequelize, DataTypes) {
+// //create the threads table
+// var Responses = sequelize.define('response', {
+
+// }, {
+//   tablename: 'responses'
+// });
+//   return Responses;
+// };
---
+++ app/models/thread.js
@@ -0,0 +1,59 @@
+
+module.exports = function(sequelize, DataTypes) {
+  	var Thread = sequelize.define('Thread', {
+	  	title: {
+			type: DataTypes.STRING
+		},
+		body: {
+		    type: DataTypes.STRING
+		}
+	});
+
+  	Thread.associate = function(models) {
+	
+		Thread.belongsTo(models.Person, {
+		  foreignKey: {
+		    allowNull: false
+		  }
+		});
+		
+	};
+
+  return Thread;
+};
+
+
+
+
+
+
+
+
+
+
+// // This may be confusing but here Sequelize (capital) references the standard library
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references our connection to the DB.
+// var sequelize = require("../config/connection.js");
+
+// var PostThreads = sequelize.define("threads", {
+//   title: {
+//     type: Sequelize.STRING
+//   },
+//   owner_id: {
+//     type: Sequelize.INTEGER
+//   },
+//   body: {
+//     type: Sequelize.STRING
+//   },
+//   created_at: {
+//     type: Sequelize.DATE
+//   }
+// }, {
+//   timestamps: false
+// });
+
+// PostThreads.sync();
+
+// module.exports = PostThreads;
+
